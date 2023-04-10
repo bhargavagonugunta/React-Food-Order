ead07372f29854531fe44c219e46967e6d55ee3a
Bhargva <bhargavagonugunta123@gmail.com>
2023-04-10 11:23:01 +0530

    master

--- src/Components/RestarentMenu.jsx
+++ src/Components/RestarentMenu.jsx
@@ -1,34 +1,36 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom"
 import useRestroMenu from "../utils/useRestroMenu";
 import { CLOUD_IMG, RESTRO_MENU } from "./Constatns";
 
 const RestarentMenu =()=>{
     const {Restid} =useParams();
     //const kid = 70704;
+    //const [restroMenu,setRestroMenu] = useState({});
+   
 
     const restrarentMenu= useRestroMenu(parseInt( Restid));
     console.log(restrarentMenu)
     console.log(Restid)
+    const menu = restrarentMenu[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards
+  //  setRestroMenu(menu);
+    console.log(menu)
    
     return (
-        <div className="Restro-menu">
-            <div>
+        <div className="flex">
+            <div className="">
             <h1>Restorent Number: {Restid}</h1>
+            <h1 className="text-xl font-serif hover:text-cyan-400">Restorent Name:  {restrarentMenu[0]?.card?.card?.info?.name}</h1>
             <h2>{restrarentMenu?.name}</h2>
-            <img src={CLOUD_IMG+RestarentMenu?.card?.card?.cloudinaryImageId} />
-            <ul>{RestarentMenu[1].card.card.itemCards.map((restro)=>{
-                return(<li >{restro.info.name}  </li>)
-            })}</ul>
-            <h2></h2>
+            <img className="w-[280px]" src={CLOUD_IMG+restrarentMenu[0]?.card?.card?.info?.cloudinaryImageId} />
             </div>
-            <div>
-                <h1>Menu</h1>
-                <ul >
-                   
-                </ul>
+            <div className="ml-6 ">
+            {menu?.map((restro)=>{
+                return(<Link to="/Cart" ><li key={restro.id} >{...restro?.card?.info?.name} </li> </Link>)
+            })}
+            
             </div>
-
+           
 
         </div>
 
--- src/utils/useRestroMenu.jsx
+++ src/utils/useRestroMenu.jsx
@@ -2,21 +2,27 @@ import { useEffect, useState } from "react"
 import { RESTRO_MENU } from "../Components/Constatns"
 
 const useRestroMenu = (restid)=>{
-    const [restroMenu,setRestroMenu] = useState([])
+    const [restroMenu,setRestroMenu] = useState({})
+  //  const [restrocard,setrestrocard] = useState([])
+
     const reid= parseInt(restid)
     console.log('useRestro is clled')
     useEffect(()=>{
         getRestroMenu()
         console.log('euse effexr')
     },[])
+    console.log(restroMenu)
+   
     async function getRestroMenu(){
         const data = await fetch(RESTRO_MENU+reid+"&submitAction=ENTER");
         const datajson = await data.json();
         console.log(datajson)
-        setRestroMenu(datajson?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
+        setRestroMenu(datajson?.data?.cards);
+       // setrestrocard(datajson?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards)
         console.log(datajson?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards)
     }
     return restroMenu;
+
 
 }
 export default useRestroMenu;
